25af4715763c3a6115bcc628ac0f0333e86590da
Jonathan-05022007 <chamaledelcidjonathaneladio@gmail.com>
2026-04-22 19:18:56 -0400

    Revert "Primeros pasos:Modelos de recursos cloud"
    This reverts commit 0bc87ee33c131435b7b07128c59749e025953a3a.

--- CloudResourceManager/CloudResourceManager/Models/ManageDataBase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CloudResourceManager.Models
-{
-    public class ManagedDatabase : CloudResource
-    {
-        public double StorageInGB { get; set; }
-        public string DatabaseType { get; set; } = string.Empty;
-    }
-}
--- CloudResourceManager/CloudResourceManager/Models/VirtualMachine.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CloudResourceManager.Models
-{
-    public class VirtualMachine : CloudResource
-    {
-        public string OperatingSystem { get; set; } = string.Empty;
-        public int RamInGB { get; set; }
-    }
-}
